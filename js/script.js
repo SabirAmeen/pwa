ab8989bcd219d225b3269a7cda3e46d60f797882
Sabir <sabir@qburst.com>
2017-03-09 17:18:14 +0530

    Modified

--- js/script.js
+++ js/script.js
@@ -1,24 +1,5 @@
 var c;
 
-navigator.credentials.get({
-    password: true,
-    unmediated: true, // request a credential without user mediation
-    federated: {
-        providers: [
-            'https://account.google.com',
-            'https://www.facebook.com'
-        ]
-    }
-}).then(function(cred) {
-    if (cred) {
-        console.log(cred.id);
-        if (cred.id == "admin") {
-            window.location = "home.html";
-        }
-    }
-})
-
-
 function check() {
     /*the following code checkes whether the entered userid and password are matching*/
     if ($("#username").val() == "admin" && $("#password").val() == "admin") {
