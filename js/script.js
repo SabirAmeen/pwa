84bb1d0a2918ed6f92c5c4ac3276651d629f4d54
Sabir <sabir@qburst.com>
2017-03-09 15:51:02 +0530

    Fixed infinite reload issue

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ navigator.credentials.get({
     }
 })
 
+
 function check() {
     /*the following code checkes whether the entered userid and password are matching*/
     if ($("#username").val() == "admin" && $("#password").val() == "admin") {
@@ -34,41 +35,4 @@ function check() {
     } else {
         alert("Error Password or Username") /*displays error message*/
     }
-}
-
-//Home Page
-$navbar = $('.nav-bar');
-$updatedarticles = $('.updated-articles');
-$transparentnav = $('.transparent-nav');
-$trendingdropdownbtn = $('#trending-dropdown-btn');
-$dropdownarrow = $('#dropdown-arrow');
-$('.menu').click(function() {
-    $navbar.show("fast");
-    $transparentnav.show();
-});
-$transparentnav.click(function() {
-    $navbar.hide("fast");
-    $updatedarticles.hide("fast");
-    $transparentnav.hide();
-});
-$('.article-toggle').click(function() {
-    $updatedarticles.show("fast");
-    $transparentnav.show();
-});
-$trendingdropdownbtn.click(function() {
-    $('#trending-dropdown').slideToggle("fast");
-    if ($dropdownarrow.text() === "\u25BC") {
-        $dropdownarrow.text("\u25B2");
-        $trendingdropdownbtn.css("background-color", "#fa4b2a");
-        $dropdownarrow.css("color", "#FFF");
-    } else {
-        $dropdownarrow.text("\u25BC");
-        $trendingdropdownbtn.css("background-color", "#f0f0f0");
-        $dropdownarrow.css("color", "#fa4b2a");
-    }
-});
-
-function signout() {
-    navigator.credentials.requireUserMediation();
-    window.location = "index.html";
 }
