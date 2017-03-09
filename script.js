cf65f6942b505c46bfa54c059546bdd0d503920e
Sabir <sabir@qburst.com>
2017-03-09 09:47:20 +0530

    initial Structure

---
+++ script.js
@@ -0,0 +1,13 @@
+function check(form)/*function to check userid & password*/
+{
+ /*the following code checkes whether the entered userid and password are matching*/
+ if(form.username.value == "admin" && form.pass.value == "admin")
+  {
+  	console.log("hi")
+ 	window.location = "home.html";/*opens the target page while Id & password matches*/
+  }
+ else
+ {
+   alert("Error Password or Username")/*displays error message*/
+  }
+}
